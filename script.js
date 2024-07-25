915b638f1d65b17358ecd47c5fa69123cc836094
Mohamed Sofy <mediumsofy@gmail.com>
2024-07-25 18:51:28 +0300

    Feat: Add grid resizing

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-const container = document.querySelector('.container')
-
-for(let i = 0; i < 16; i++)
-{
-    for(let j = 0; j < 16; j++)
-    {
-        let div = document.createElement('div')
-        div.classList.add('pixel')
-        container.appendChild(div)
-    }
+const container = document.querySelector(".container");
+const button = document.querySelector("button");
+let gridSize = 0;
+let pixels = "";
+// initial 16x16 grid
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    let div = document.createElement("div");
+    div.classList.add("pixel");
+    div.style.cssText = `flex-basis: ${500 / 16 - 2}px; height: ${
+      500 / 16 - 2
+    }px`;
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "red";
+    });
+    container.appendChild(div);
+  }
 }
 
-const pixels = document.querySelectorAll('.pixel')
-console.log(pixels)
-pixels.forEach((pixel)=>{
-
-    pixel.addEventListener("mouseover" , () => {
-        pixel.style.backgroundColor = 'red' 
-    })
-})
+// computed grid based on user input
+button.addEventListener("click", () => {
+  gridSize = +prompt("Enter the new size of the canvas");
+  container.innerHTML = "";
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      let div = document.createElement("div");
+      div.classList.add("pixel");
+      div.style.cssText = `flex-basis: ${Math.ceil(
+        500 / gridSize - 2
+      )}px; height: ${Math.ceil(500 / gridSize - 2)}px`;
+      div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "red";
+      });
+      container.appendChild(div);
+    }
+  }
+});
